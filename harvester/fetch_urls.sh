6bdc74265573b5e3962c05331e2fb4ad042de802
Tobias Schweizer <tobias.schweizer@switch.ch>
2023-03-23 17:16:52 +0100

    refactor (fetch download urls): fetch several pages

--- harvester/fetch_urls.sh
+++ harvester/fetch_urls.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-curl "https://ckan.opendata.swiss/api/3/action/package_search?fq=groups:territory&rows=1000" | jq '[.result.results[] | .resources[] | select(.format == "CSV") | .download_url]' > territory.json
+for i in {0..5}; do
 
-curl "https://ckan.opendata.swiss/api/3/action/package_search?fq=groups:geography&rows=1000" | jq '[.result.results[] | .resources[] | select(.format == "CSV") | .download_url]' > geography.json
+    curl "https://ckan.opendata.swiss/api/3/action/package_search?fq=groups:territory&rows=1000&start=$i" | jq '[.result.results[] | .resources[] | select(.format == "CSV") | .download_url]' > territory_$i.json
+
+    curl "https://ckan.opendata.swiss/api/3/action/package_search?fq=groups:geography&rows=1000&start=$i" | jq '[.result.results[] | .resources[] | select(.format == "CSV") | .download_url]' > geography_$i.json
+
+done

bb7ec3f643af6ad59bdfe03c76c19920661b7887
hurni <71129916+hurni@users.noreply.github.com>
2023-03-24 11:59:58 +0100

    Update enrich_metadata.py

--- entich_metadata/enrich_metadata.py
+++ entich_metadata/enrich_metadata.py
@@ -1,33 +1,28 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 24 11:38:32 2023
-
-@author: haema
-"""
-
 
 import requests as r
 import urllib3
 
-
-
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 s = r.Session()
-
 proxies = None
 private_api_key = # your API Key from useraccount on backend
 patch_url = 'https://ckan.ogdch-abnahme.clients.liip.ch/api/3/action/package_patch'
 
 header = {'Authorization': private_api_key}
 
-
-
-# list of adapted ids = 
 # add gemeindenames as dct:spatial.  
+"""
+list of package_ids
+for id in package_ids:
+    add d.key(value)
+
+
+"""
 
 data = {
-        'id': "6fbab091-823e-43df-8b1d-de2c3e6763c9",
+        'id': "6fbab091-823e-43df-8b1d-de2c3e6763c9", # Example. For automatisation: add list above
         'spatial' : 'Zollikofen'
         }
 

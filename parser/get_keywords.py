d9aaf0856c405ed49cb50d967dc02f38eb207256
hesterfrederiek <hesterpieters@gmail.com>
2023-03-24 09:37:48 +0100

    add get_encoding

--- parser/get_keywords.py
+++ parser/get_keywords.py
@@ -1,5 +1,6 @@
 import csv
 import pandas as pd
+import chardet
 
 #get the delimiter of the csv-file
 #https://stackoverflow.com/questions/46135839/auto-detect-the-delimiter-in-a-csv-file-using-pd-read-csv
@@ -11,5 +12,14 @@ def get_delimiter(file_path, bytes = 4096):
     return delimiter
 
 #example
+
+# if not utf-8....
+# look at the first ten thousand bytes to guess the character encoding
+def get_encoding(filename):
+    with open(filename, 'rb') as rawdata:
+        result = chardet.detect(rawdata.read(10000))
+        encoding = result['encoding']
+    return encoding
+
 
 df = pd.read_csv(exampleFile, sep = get_delimiter(exampleFile))

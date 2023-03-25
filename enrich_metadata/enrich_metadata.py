aee45c7fe83f71a32d53508f6427e58fcc62466e
hurni <71129916+hurni@users.noreply.github.com>
2023-03-25 12:16:09 +0100

    Update enrich_metadata.py

--- enrich_metadata/enrich_metadata.py
+++ enrich_metadata/enrich_metadata.py
@@ -1,78 +1,31 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 24 11:38:32 2023
-
-@author: haema
-"""
-
 
 import requests as r
 import urllib3
-
+import pandas as pd
 
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+df = pd.read_csv("https://raw.githubusercontent.com/hurni/GovTech/main/export/test_final.csv")
+
 s = r.Session()
 
 proxies = None
-private_api_key = '0887bd41-1637-4334-a0ad-633b0f557667'
-base_url = 'https://ckan.ogdch-abnahme.clients.liip.ch/api/3/action/package_patch'
+
+private_api_key = YOUR-PRIVATE-API_KEY # see your Accountinformation on opendata.swiss
+
+patch_url = 'https://ckan.opendata.swiss/api/3/action/package_patch'
 
 header = {'Authorization': private_api_key}
 
-
-
-# list of adapted ids = 
-# add gemeindenames as dct:spatial.  
-
-data = {
-        'id': "6fbab091-823e-43df-8b1d-de2c3e6763c9",
-        'spatial' : 'Zollikofen'
-        }
-
-
-x = s.post(base_url,data=data,proxies=proxies,headers=header,verify=False)
-
-print(x.status_code)
-
-
-"""
-
-# -*- coding: utf-8 -*-
-
-import requests as r
-import urllib3
-
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-s = r.Session()
-proxies = None
-private_api_key = # your API Key from useraccount on backend
-patch_url = 'https://ckan.ogdch-abnahme.clients.liip.ch/api/3/action/package_patch'
-
-header = {'Authorization': private_api_key}
-
-# add gemeindenames as dct:spatial.  
-"""
-list of package_ids
-for id in package_ids:
-    add d.key(value)
-
-
-"""
-
-data = {
-        'id': "6fbab091-823e-43df-8b1d-de2c3e6763c9", # Example. For automatisation: add list above
-        'spatial' : 'Zollikofen'
-        }
-
-
-x = s.post(patch_url,data=data,proxies=proxies,headers=header,verify=False)
-
-# print(x.status_code)
-
-
-"""
-
-
+for index, row in df.iterrows():
+      
+    data = {
+            'id': str(row['package_id']),
+            'spatial' : str(row['communities_names'])
+            }
+    
+    #print(data)
+    
+    x = s.post(patch_url,data=data,proxies=proxies,headers=header,verify=False)
+    print(x.status_code)
